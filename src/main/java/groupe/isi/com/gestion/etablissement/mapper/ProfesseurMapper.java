3ef676168f38f00df8013e11d64d6aba45ce6096
keyena1 <s.leye3@isepdiamniation.edu.sn>
2025-03-09 12:42:44 +0100

    Fix: Update MapStruct and Lombok configuration

--- src/main/java/groupe/isi/com/gestion/etablissement/mapper/ProfesseurMapper.java
+++ src/main/java/groupe/isi/com/gestion/etablissement/mapper/ProfesseurMapper.java
@@ -4,8 +4,9 @@ import groupe.isi.com.gestion.etablissement.dto.ProfesseurDto;
 import groupe.isi.com.gestion.etablissement.model.Professeur;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProfesseurMapper {
     @Mapping(target = "id", ignore = true)
     Professeur toEntity(ProfesseurDto dto);
